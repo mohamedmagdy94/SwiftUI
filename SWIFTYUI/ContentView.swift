22bda3f5ce8a566f1b2b654a9791fe058f5b7649
Mohamed ElTaweel <mohamedmagdy.9080@gmail.com>
2020-09-21 18:21:29 +0200

    Initial Project

---
+++ SWIFTYUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SWIFTYUI
+//
+//  Created by Mohamed El-Taweel on 9/21/20.
+//  Copyright © 2020 Learning. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
